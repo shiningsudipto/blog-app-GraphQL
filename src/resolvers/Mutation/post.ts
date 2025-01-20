10b1e9b84429518ea24782e0be686ecad49b5f39
Sudipta Das <shiningsudipto@gmail.com>
2025-01-20 09:06:07 +0600

    post updating

--- src/resolvers/Mutation/post.ts
+++ src/resolvers/Mutation/post.ts
@@ -18,4 +18,26 @@ export const postResolvers = {
       data: newPost,
     };
   },
+  updatePost: async (parent: any, args: any, { prisma, userInfo }: any) => {
+    console.log(args, userInfo);
+    if (!userInfo) {
+      return {
+        error: "Unauthorized",
+        data: null,
+      };
+    }
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: Number(args.postId),
+      },
+      data: args.post,
+    });
+
+    console.log(updatedPost);
+
+    return {
+      userError: null,
+      data: updatedPost,
+    };
+  },
 };
