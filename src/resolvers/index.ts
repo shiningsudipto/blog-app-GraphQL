af369d8f64a2a731b9080a874b996f6e89c5be8e
Sudipta Das <shiningsudipto@gmail.com>
2025-01-21 22:11:29 +0600

    user and profile relation added

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,9 +1,13 @@
 import { Query } from "./Query";
 import { Mutation } from "./Mutation";
 import { Post } from "./Relations/post";
+import { User } from "./Relations/user";
+import { Profile } from "./Relations/profile";
 
 export const resolvers = {
   Query,
   Post,
+  User,
+  Profile,
   Mutation,
 };
