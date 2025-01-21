8b6f561b8b870201b3fe91a19410a7cc6e761495
Sudipta Das <shiningsudipto@gmail.com>
2025-01-21 09:34:14 +0600

    post relations added

---
+++ src/resolvers/Relations/post.ts
@@ -0,0 +1,5 @@
+export const Post = {
+  author: async (parent: any, args: any, { prisma, userInfo }: any) => {
+    return await prisma.user.findUnique({ where: { id: parent.authorId } });
+  },
+};
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,9 @@
 import { Query } from "./Query";
 import { Mutation } from "./Mutation";
+import { Post } from "./Relations/post";
 
 export const resolvers = {
   Query,
+  Post,
   Mutation,
 };
